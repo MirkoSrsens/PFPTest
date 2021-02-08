3af763302545f2a933c294a08bc983fede7afce3
mirkorawfury <mirko@rawfury.com>
2021-02-08 16:19:00 +0100

    Pathfinding

---
+++ ProjectTest/Assets/Scripts/Enemy/Pathfinding.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Pathfinding : MonoBehaviour
+{
+    [SerializeField]
+    private Tilemap tileMap;
+    private Vector2Int[,] cells;
+
+    private void Awake()
+    {
+        var start = tileMap.cellBounds.min;
+        var end = tileMap.cellBounds.max;
+        var size = start - end;
+        cells = new Vector2Int[Mathf.Abs(size.x), Mathf.Abs(size.y)];
+
+        for(int i =0; i<cells.GetLength(0); i++)
+        {
+            for (int j = 0; j < cells.GetLength(1); j++)
+            {
+                cells[i,j] = new Vector2Int(i, j);
+            }
+        }
+    }
+
+    public static void GetPath(this Transform current, Transform target)
+    {
+
+    }
+}
